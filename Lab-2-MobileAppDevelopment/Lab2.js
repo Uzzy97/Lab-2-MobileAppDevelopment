993d788d91292b546a7fd5841bbd5835ab3cfb5c
uzzy97 <G00345816@gmit.ie>
2018-01-29 17:31:11 +0000

    Lab 2 Question 2

---
+++ Lab-2-MobileAppDevelopment/Lab2.js
@@ -0,0 +1,21 @@
+var tasks = [];
+function addTask(task) {
+    var count;
+    tasks.push(task);
+    console.log("New Task: " + task + "Added");
+    count = task.length;
+    return count;
+}
+function listAllTasks() {
+    tasks.forEach(function (task) {
+        console.log(task);
+    });
+}
+function deleteTask(task) {
+    var key = task;
+    var index = tasks.lastIndexOf(key, 0);
+    if (index > -1) {
+        tasks.splice(index, 1);
+    }
+    return tasks.length;
+}
--- Lab-2-MobileAppDevelopment/Lab2.ts
+++ Lab-2-MobileAppDevelopment/Lab2.ts
@@ -1,4 +1,28 @@
-var stringArray:string[]; 
-function addTask(value: string){
+let tasks: Array<string> = [];
+function addTask(task:string):  number{
+
+    let count: number;
+    tasks.push(task)
+    console.log("New Task: " + task + "Added");
+    count = task.length;
+    return count
+}
+
+function listAllTasks(){
+    tasks.forEach(function(task){
+        console.log(task)
+    })
 
 }
+
+function deleteTask(task:string): number{
+
+    let key: string = task;
+    let index: number = tasks.lastIndexOf(key,0);
+        if(index > -1)
+        {
+            tasks.splice(index, 1);
+        }
+        return tasks.length
+}
+
